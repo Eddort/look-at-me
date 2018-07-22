5b5c5f872fbfeca0810b289081c99c32cecf6331
Eddort <webisforce@gmail.com>
2018-07-22 09:37:39 +0300

    commader is added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export default (a: number, b: number): number => {
-	return a + b;
-};
+import * as program from "commander";
+
+program
+	.command("rm <dir>")
+	.option("-r, --recursive", "Remove recursively")
+	.action((dir: string, cmd: any) => {
+		console.log("resmove " + dir + (cmd.recursive ? " recursively" : ""));
+	});
+
+program.parse(process.argv);
